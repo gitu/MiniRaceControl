cf9dea968c2fee29ffc0b926b031396b3a96bb26
Florian Schrag <florian@schrag.org>
2015-04-22 16:49:01 +0200

    playing with pygame

--- PiRace.py
+++ PiRace.py
@@ -243,7 +243,7 @@ while True:
 
     if screenMode == 0:
         for idx, result in enumerate(rounds[::-1]):
-            text = myfont.render(result['car'] + ' - ' + str(result['time'] / 1000.0) + 's', 1, (10, 10, 10))
+            text = myfont.render('car ' + str(result['car']) + ' - ' + str(result['time'] / 1000.0) + 's', 1, (10, 10, 10))
             textpos = text.get_rect()
             textpos.centerx = screen.get_rect().centerx
             textpos.top = idx * myfont.get_linesize() + 60

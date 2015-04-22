290045d074b5b25647857c25d057ce45ae5afde6
Florian Schrag <florian@schrag.org>
2015-04-22 16:46:36 +0200

    playing with pygame

--- PiRace.py
+++ PiRace.py
@@ -215,7 +215,7 @@ while True:
     # Process touchscreen input
     while True:
         while rt.read_track(False):
-            print(".")
+            pass
         screen_change = 0
         for event in pygame.event.get():
             if (event.type is MOUSEBUTTONDOWN):
@@ -243,7 +243,6 @@ while True:
 
     if screenMode == 0:
         for idx, result in enumerate(rounds[::-1]):
-            print(result[0])
             text = myfont.render(result.car + ' - ' + str(result.time / 1000.0) + 's', 1, (10, 10, 10))
             textpos = text.get_rect()
             textpos.centerx = screen.get_rect().centerx
